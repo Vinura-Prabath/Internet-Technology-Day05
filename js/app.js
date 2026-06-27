903542f5f8e7f6120f5773ef6198bd0cf53f834c
Vinura-Prabath <vinurakumarathunga8866@gmail.com>
2026-06-27 15:22:43 +0530

    Add function to sum two numbers and log the result in app.js

--- js/app.js
+++ js/app.js
@@ -111,5 +111,24 @@
 // } while (i <= 10);
 
 
+// -------------------------------------------
+
+// function
+
+function add(x, y) {
+    return x + y;
+}
+
+let result = add(10, 20);
+console.log(result);
+
+
+// public static String add(int x, int y) {
+//     return x + y;
+// }
+
+// -------------------------------------------
+
+
 
 
